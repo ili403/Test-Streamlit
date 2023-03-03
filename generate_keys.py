e1ca1404682d128d5b1b34bbb1e4d19b6b60a505
ili403 <ineslabidi@citycable.ch>
2023-03-03 19:19:53 +0100

    Authentification

---
+++ generate_keys.py
@@ -0,0 +1,12 @@
+import pickle
+from pathlib import path
+import streamlit_authenticator as stauth
+
+names = ["Steve Brodbeck", "Antoine Desbiolles","Ines Labidi"]
+usernames = ["sbrodbeck", "adesbiolles", "ilabidi"]
+passwords = ["123", "456", "789"]
+
+hashed_passwords = stauth.Hasher(passwords).generate()
+file_path = Path(__file__).parent / "hashed_pw.pk1"
+with file_path.open("wb") as file:
+    pickle.dump(hashed_passwords, file)
